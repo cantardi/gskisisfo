a651a1bb49d31ee3351eadbc1df2b538af6f1a09
cantardi <cantabile.ardi@gmail.com>
2019-04-29 21:48:11 +0700

    Enter

--- src/components/Songs/SongLP.js
+++ src/components/Songs/SongLP.js
@@ -1,14 +1,13 @@
 import React, { Component } from 'react';
 import { Container } from 'react-bootstrap';
-import SongAction from "./SongAction";
 import SongSearch from "./SongSearch";
 import SongResult from "./SongResult";
 
 class SongLP extends Component {
+
   render() {
     return (
       <Container className="pa2">
-        <SongAction />
         <SongSearch />
         <SongResult />
       </Container>
