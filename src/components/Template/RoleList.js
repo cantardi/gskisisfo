267a581b7882f2c8cd94ba3aade9ea1d677e4938
cantardi <cantabile.ardi@gmail.com>
2019-10-28 23:47:11 +0700

    update the localhost

--- src/components/Template/RoleList.js
+++ src/components/Template/RoleList.js
@@ -95,7 +95,7 @@ class RoleList extends Component {
     let newRoleName = []
     addedRole.map(role => newRoleName.push(Object ({rolename: role.rolename}) ))
 
-    fetch(process.env.BACKEND_URL + 'addnewrole', {
+    fetch('https://gskisisfobackend.herokuapp.com/addnewrole', {
       method: 'post',
       headers: {'Content-Type': 'application/json'},
       body: JSON.stringify({
@@ -117,7 +117,7 @@ class RoleList extends Component {
 
   deleteRoleRequest = () => {
 
-    fetch(process.env.BACKEND_URL + 'deleterole', {
+    fetch('https://gskisisfobackend.herokuapp.com/deleterole', {
       method: 'delete',
       headers: {'Content-Type': 'application/json'},
       body: JSON.stringify({
@@ -153,7 +153,7 @@ class RoleList extends Component {
   }
 
   componentDidMount(){
-    fetch(process.env.BACKEND_URL+'getchurchrole', {
+    fetch('https://gskisisfobackend.herokuapp.com/getchurchrole', {
       method: 'get',
       headers: {'Content-Type': 'application/json'}
     })
