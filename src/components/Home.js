6d2b77b839c9c9e8cf91a9362c46cc8c5e9adaf9
cantardi <cantabile.ardi@gmail.com>
2019-11-21 09:23:30 +0700

    test

--- src/components/Home.js
+++ src/components/Home.js
@@ -5,22 +5,29 @@ import {userContext} from '../helpers/userContext';
 class Home extends Component {
   render() {
     return (
-      <Carousel>
+      <div>
+        <Carousel>
+          <div>
+            <img alt="foto1" src="https://i.gocollette.com/tour-media-manager/tours/europe/france/638/packages/master-package/top-carousel/labelleseine-hero1-v2.jpg" />
+          </div>
+          <div>
+            <img alt="foto2" src="http://www.magnum.com.my/assets/images/main_Carousel_01.jpg" />
+          </div>
+          <div>
+            <img alt="foto3" src="https://i.gocollette.com/tour-media-manager/tours/europe/norway/102/packages/master-package/top-carousel/spectacularscandinavia_hero1_norway.jpg" />
+          </div>
+          <div>
+            
+          </div>
+        </Carousel>
         <div>
-          <img alt="foto1" src="https://i.gocollette.com/tour-media-manager/tours/europe/france/638/packages/master-package/top-carousel/labelleseine-hero1-v2.jpg" />
-        </div>
-        <div>
-          <img alt="foto2" src="http://www.magnum.com.my/assets/images/main_Carousel_01.jpg" />
-        </div>
-        <div>
-          <img alt="foto3" src="https://i.gocollette.com/tour-media-manager/tours/europe/norway/102/packages/master-package/top-carousel/spectacularscandinavia_hero1_norway.jpg" />
-        </div>
-        <div>
+          My user name is:
           <userContext.Consumer>
             { user => user.username }
           </userContext.Consumer>
         </div>
-      </Carousel>
+      </div>
+      
     );
   }
 }
