14f039eb59cae7a65ea1de4c9c83c26dcb6ec3da
cantardi <cantabile.ardi@gmail.com>
2020-01-07 21:04:12 +0700

    update pdf padding

--- src/components/_reusables/R_SongPdf.js
+++ src/components/_reusables/R_SongPdf.js
@@ -36,7 +36,7 @@ const styles = StyleSheet.create({
     fontSize: 24,
   },
   dateTitle: {
-    fontSize: 12,
+    fontSize: 14,
     padding: 5,
     textAlign: "center",
     fontFamily: 'Helvetica'
